17afbf2c8fa97251c145d6a390210b50f38e8851
= <=>
2019-02-18 17:38:47 -0300

    aula 24 - Melhorar exibição de mensagens

--- app/Http/Controllers/admin/BalanceController.php
+++ app/Http/Controllers/admin/BalanceController.php
@@ -28,6 +28,14 @@ class BalanceController extends Controller
     public function depositStore(MoneyValidationFormRequest $request)
     {
         $balance = auth()->user()->balance()->firstOrCreate([]);
-        dd($balance->deposit($request->value));
+        $response = $balance->deposit($request->value);
+
+        if($response['success'])
+            return redirect()
+                ->route('admin.balance')
+                ->with('success', $response['message']);
+        return redirect()
+                ->back()
+                ->with('error', $response['message']);
     }
 }
